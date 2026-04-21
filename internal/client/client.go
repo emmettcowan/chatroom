05faaadba127b59669a1c5dadb217d189553d881
Emmett Cowan <knockanillaun@gmail.com>
2026-04-21 17:27:37 +0100

    inital bubble tea text area

--- internal/client/client.go
+++ internal/client/client.go
@@ -5,8 +5,68 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"os"
+
+	"charm.land/bubbles/v2/textinput"
+	tea "charm.land/bubbletea/v2"
+	"charm.land/lipgloss/v2"
 )
+
+type model struct {
+	textInput textinput.Model
+	err       error
+	quitting  bool
+}
+
+func initialModel() model {
+	ti := textinput.New()
+	ti.Placeholder = "Pikachu"
+	ti.SetVirtualCursor(false)
+	ti.Focus()
+	ti.CharLimit = 156
+	ti.SetWidth(20)
+
+	return model{textInput: ti}
+}
+
+func (m model) Init() tea.Cmd {
+	return textinput.Blink
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyPressMsg:
+		switch msg.String() {
+		case "enter", "ctrl+c", "esc":
+			m.quitting = true
+			return m, tea.Quit
+		}
+	}
+
+	m.textInput, cmd = m.textInput.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() tea.View {
+	var c *tea.Cursor
+	if !m.textInput.VirtualCursor() {
+		c = m.textInput.Cursor()
+		c.Y += lipgloss.Height(m.headerView())
+	}
+
+	str := lipgloss.JoinVertical(lipgloss.Top, m.headerView(), m.textInput.View(), m.footerView())
+	if m.quitting {
+		str += "\n"
+	}
+
+	v := tea.NewView(str)
+	v.Cursor = c
+	return v
+}
+
+func (m model) headerView() string { return "What’s your favorite Pokémon?\n" }
+func (m model) footerView() string { return "\n(esc to quit)" }
 
 func Run() {
 	conn, err := net.Dial("tcp", ":8090")
@@ -16,24 +76,28 @@ func Run() {
 	defer conn.Close()
 
 	go listenToServer(conn)
-
-	reader := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Print(">: ")
-		text, err := reader.ReadString('\n')
-		if err != nil {
-			if err != os.ErrClosed {
-				log.Printf("Error reading stdin: %v", err)
-			}
-			break
-		}
-
-		_, err = conn.Write([]byte(text))
-		if err != nil {
-			fmt.Printf("Error writing to connection: %v\n", err)
-			break
-		}
+	p := tea.NewProgram(initialModel())
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
 	}
+
+	// reader := bufio.NewReader(os.Stdin)
+	// for {
+	// 	fmt.Print(">: ")
+	// 	text, err := reader.ReadString('\n')
+	// 	if err != nil {
+	// 		if err != os.ErrClosed {
+	// 			log.Printf("Error reading stdin: %v", err)
+	// 		}
+	// 		break
+	// 	}
+	//
+	// 	_, err = conn.Write([]byte(text))
+	// 	if err != nil {
+	// 		fmt.Printf("Error writing to connection: %v\n", err)
+	// 		break
+	// 	}
+	// }
 }
 
 func listenToServer(conn net.Conn) {
